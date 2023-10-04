4e1401e3c5322164155c8494a60ffdf7293eaa9d
Rdagenais20 <rdagenais@oakland.edu>
2023-10-04 17:13:50 -0400

    Mostly complete
    Added getFileStrings and began getFileChars. Almost done. Also touched up the txt files

--- main.c
+++ main.c
@@ -1,6 +1,64 @@
-#include <stdio>
+#include <stdio.h>
+
+void getFileStrings(char output[][20], int size, char filename[]) {
+	//declaring variables
+	int i = 0;
+
+	//get file pointer
+	FILE* fptr = fopen(filename, "r");
+
+	//test if file opened successfully
+	if (fptr == NULL) {
+		printf("The file is not opened. The program will now exit.");
+	}
+
+	//getting data from file
+	do {
+		fgets(output[i], size, fptr);
+		i++;
+	} while (output[i-1][0] != '\0');
+
+	fclose(fptr);
+}
+
+void getFileChars(char* output, char filename[]) {
+	//declaring variables
+	int i = 0;
+
+	//get file pointer
+	FILE* fptr = fopen(filename, "r");
+
+	//test if file opened successfully
+	if (fptr == NULL) {
+		printf("The file is not opened. The program will now exit.");
+	}
+
+	//getting data from file
+	do {
+		output[i] = fgetc(fptr);
+		printf("%c", output[i]);
+		i++;
+	} while (output[i-1] != '');
+
+	fclose(fptr);
+}
 
 int main() {
+	//declaring variables
+	char d1[200][20], d2[200][20], d3[200][20], d4[200][20], stop[700][20], special[1000];
+	int i = 0;
 
+	//getting file contents and putting them into arrays
+	getFileStrings(d1, 20, "d1.txt");
+	getFileStrings(d2, 20, "d2.txt");
+	getFileStrings(d3, 20, "d3.txt");
+	getFileStrings(d4, 20, "d4.txt");
+	getFileStrings(stop, 20, "stopwords.txt");
+	getFileChars(special, "specialcharacters.txt");
+
+	while (special[i] != '\0') {
+		printf("%c", special[i]);
+		i++;
+	}
 	return 0;
 }
